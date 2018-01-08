19e5205a92f02728b08ca64edac021908b01ced4
Tim Otten <totten@civicrm.org>
2018-01-08 15:10:53 -0800

    FileUtil - Add a couple oddball helper functions

---
+++ src/Setup/FileUtil.php
@@ -0,0 +1,29 @@
+<?php
+namespace Civi\Setup;
+
+class FileUtil {
+
+  public static function isCreateable($file) {
+    if (file_exists($file)) {
+      return is_writable($file);
+    }
+
+    $next = dirname($file);
+    do {
+      $current = $next;
+      if (file_exists($current)) {
+        return is_writable($current);
+      }
+      $next = dirname($current);
+    } while ($current && $next && $current != $next);
+
+    return FALSE;
+  }
+
+  public static function makeWebWriteable($path) {
+    // Blerg: Setting world-writable works as a default, but
+    // it 'sprone to break systems that rely on umask's or facl's.
+    chmod($path, 0777);
+  }
+
+}
