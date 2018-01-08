6e39432972e1476486bb450c3d0fad5509b45e8f
Tim Otten <totten@civicrm.org>
2018-01-08 15:11:47 -0800

    Add TemplateCompilePath.civi-setup.php

---
+++ plugins/TemplateCompilePath.civi-setup.php
@@ -0,0 +1,28 @@
+<?php
+if (!defined('CIVI_SETUP')) {
+  exit();
+}
+
+\Civi\Setup::instance()->getDispatcher()
+  ->addListener('civi.setup.checkRequirements', function (\Civi\Setup\Event\CheckRequirementsEvent $e) {
+    $m = $e->getModel();
+
+    if (empty($m->templateCompilePath)) {
+      $e->addError('templateCompilePath', sprintf('The templateCompilePath is undefined.'));
+    }
+
+    $e->addMessage('templateCompilePathWritable', sprintf('The template compile dir "%s" cannot be created. Ensure the parent folder is writable.', $m->templateCompilePath), \Civi\Setup\FileUtil::isCreateable($m->templateCompilePath));
+  });
+
+\Civi\Setup::instance()->getDispatcher()
+  ->addListener('civi.setup.installSettings', function (\Civi\Setup\Event\InstallSettingsEvent $e) {
+    $m = $e->getModel();
+
+    if (!file_exists($m->templateCompilePath)) {
+      Civi\Setup::log()->info('[TemplateCompilePath] mkdir "{path}"', [
+        'path' => $m->templateCompilePath,
+      ]);
+      mkdir($m->templateCompilePath, 0777, TRUE);
+      \Civi\Setup\FileUtil::makeWebWriteable($m->templateCompilePath);
+    }
+  });
