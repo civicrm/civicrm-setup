1af7481b4ce57d275b9c4504ccf8657f543519c4
Tim Otten <totten@civicrm.org>
2018-01-10 08:41:53 -0800

    Model - Add $lang and $loadGenerated

--- src/Setup/Model.php
+++ src/Setup/Model.php
@@ -28,6 +28,11 @@ namespace Civi\Setup;
  *   Ex: ['server'=>'localhost:3306', 'username'=>'admin', 'password'=>'s3cr3t', 'database'=>'mydb']
  * @property string $siteKey
  *   Ex: 'abcd1234ABCD9876'.
+ * @property string|NULL $lang
+ *   The language of the default dataset.
+ *   Ex: 'fr_FR'.
+ * @property bool $loadGenerated
+ *   EXPERIMENTAL/DEPRECATED: Load example dataset (in lieu of standard empty dataset).
  * @property array $components
  *   Ex: ['CiviMail', 'CiviContribute', 'CiviEvent', 'CiviMember', 'CiviReport']
  * @property array $extensions
@@ -99,6 +104,16 @@ class Model {
     $this->addField(array(
       'description' => 'Site key',
       'name' => 'siteKey',
+      'type' => 'string',
+    ));
+    $this->addField(array(
+      'description' => 'Load example data',
+      'name' => 'loadGenerated',
+      'type' => 'bool',
+    ));
+    $this->addField(array(
+      'description' => 'Language',
+      'name' => 'lang',
       'type' => 'string',
     ));
     $this->addField(array(
