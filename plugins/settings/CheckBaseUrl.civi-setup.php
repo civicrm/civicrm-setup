84c47ed7edfc9f36b322b1eaf0863f4ed67d89e4
Tim Otten <totten@civicrm.org>
2018-01-10 14:27:00 -0800

    settings/CheckBaseUrl - Generate a warning if the autodetected URL looks bad.

---
+++ plugins/settings/CheckBaseUrl.civi-setup.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @file
+ *
+ * Verify that the CMS base URL is well-formed.
+ *
+ * Ex: When installing via CLI, the URL cannot be determined automatically.
+ */
+
+if (!defined('CIVI_SETUP')) {
+  exit();
+}
+
+\Civi\Setup::dispatcher()
+  ->addListener('civi.setup.checkRequirements', function (\Civi\Setup\Event\CheckRequirementsEvent $e) {
+    $model = $e->getModel();
+
+    if (!$model->cmsBaseUrl || !preg_match('/^https?:/', $model->cmsBaseUrl)) {
+      $e->addError('cmsBaseUrl', "The \"cmsBaseUrl\" ($model->cmsBaseUrl) is unavailable or malformed. Consider setting it explicitly.");
+    }
+    else {
+      $e->addOk('cmsBaseUrl', 'The "cmsBaseUrl" appears well-formed.');
+    }
+  });
