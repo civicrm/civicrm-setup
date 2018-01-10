ec463554b674e29882ceacab97688a6dcbc5dbf3
Tim Otten <totten@civicrm.org>
2018-01-10 08:39:52 -0800

    DbUtil - Add execute, fetchAll, findTables, findViews. Split connect/softConnect. Throw SqlException.

--- src/Setup/DbUtil.php
+++ src/Setup/DbUtil.php
@@ -1,6 +1,8 @@
 <?php
 namespace Civi\Setup;
 
+use Civi\Setup\Exception\SqlException;
+
 class DbUtil {
 
   /**
@@ -21,7 +23,7 @@ class DbUtil {
    * @param array $db
    * @return \mysqli
    */
-  public static function connect($db) {
+  public static function softConnect($db) {
     $host = $db['server'];
     $hostParts = explode(':', $host);
     if (count($hostParts) > 1 && strrpos($host, ']') !== strlen($host) - 1) {
@@ -37,15 +39,25 @@ class DbUtil {
 
   /**
    * @param array $db
+   * @return \mysqli
+   * @throws SqlException
+   */
+  public static function connect($db) {
+    $conn = self::softConnect($db);
+    if (mysqli_connect_errno()) {
+      throw new SqlException(sprintf("Connection failed: %s\n", mysqli_connect_error()));
+    }
+    return $conn;
+  }
+
+  /**
+   * @param array $db
    * @param string $fileName
    * @param bool $lineMode
    *   What does this mean? Seems weird.
    */
   public static function sourceSQL($db, $fileName, $lineMode = FALSE) {
     $conn = self::connect($db);
-    if (mysqli_connect_errno()) {
-      throw new \Exception(sprintf("Connection failed: %s\n", mysqli_connect_error()));
-    }
 
     mysqli_free_result($conn->query('SET NAMES utf8'));
 
@@ -68,7 +80,7 @@ class DbUtil {
             mysqli_free_result($result);
           }
           else {
-            throw new \Exception("Cannot execute $query: " . mysqli_error($conn));
+            throw new SqlException("Cannot execute $query: " . mysqli_error($conn));
           }
         }
       }
@@ -85,11 +97,97 @@ class DbUtil {
             mysqli_free_result($result);
           }
           else {
-            throw new \Exception("Cannot execute $string: " . mysqli_error($conn));
+            throw new SqlException("Cannot execute $string: " . mysqli_error($conn));
           }
         }
       }
     }
+  }
+
+  /**
+   * Execute query. Ignore the results.
+   *
+   * @param \mysqli|array $conn
+   *   The DB to query. Either a mysqli connection, or credentials for
+   *    establishing one.
+   * @param string $sql
+   * @throws SqlException
+   */
+  public static function execute($conn, $sql) {
+    $conn = is_array($conn) ? self::connect($conn) : $conn;
+    $result = $conn->query($sql);
+    if (!$result) {
+      throw new SqlException("Cannot execute $sql: " . $conn->error);
+    }
+
+    if ($result && $result !== TRUE) {
+      $result->free_result();
+    }
+
+  }
+
+  /**
+   * Get all the results of a SQL query, as an array.
+   *
+   * @param \mysqli|array $conn
+   *   The DB to query. Either a mysqli connection, or credentials for
+   *    establishing one.
+   * @param string $sql
+   * @return array
+   * @throws \Exception
+   */
+  public static function fetchAll($conn, $sql) {
+    $conn = is_array($conn) ? self::connect($conn) : $conn;
+    $result = $conn->query($sql);
+    if (!$result) {
+      throw new SqlException("Cannot execute $sql: " . $conn->error);
+    }
+
+    $rows = array();
+    while ($row = $result->fetch_assoc()) {
+      $rows[] = $row;
+    }
+    $result->free_result();
+
+    return $rows;
+  }
+
+  /**
+   * Get a list of views in the given database.
+   *
+   * @param \mysqli|array $conn
+   *   The DB to query. Either a mysqli connection, or credentials for
+   *    establishing one.
+   * @param string $databaseName
+   * @return array
+   *   Ex: ['civicrm_view1', 'civicrm_view2']
+   */
+  public static function findViews($conn, $databaseName) {
+    $sql = sprintf("SELECT table_name FROM information_schema.TABLES  WHERE TABLE_SCHEMA='%s' AND TABLE_TYPE = 'VIEW'",
+      $conn->escape_string($databaseName));
+
+    return array_map(function($arr){
+      return $arr['table_name'];
+    }, self::fetchAll($conn, $sql));
+  }
+
+  /**
+   * Get a list of concrete tables in the given database.
+   *
+   * @param \mysqli|array $conn
+   *   The DB to query. Either a mysqli connection, or credentials for
+   *    establishing one.
+   * @param string $databaseName
+   * @return array
+   *   Ex: ['civicrm_view1', 'civicrm_view2']
+   */
+  public static function findTables($conn, $databaseName) {
+    $sql = sprintf("SELECT table_name FROM information_schema.TABLES  WHERE TABLE_SCHEMA='%s' AND TABLE_TYPE = 'BASE TABLE'",
+      $conn->escape_string($databaseName));
+
+    return array_map(function($arr){
+      return $arr['table_name'];
+    }, self::fetchAll($conn, $sql));
   }
 
 }
