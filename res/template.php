e5453b51ca961e5023d871cd878031a2d4563f78
Tim Otten <totten@civicrm.org>
2018-01-16 09:05:01 -0800

    template.php - Fix installer animation

--- res/template.php
+++ res/template.php
@@ -52,12 +52,10 @@ $mainClasses = array(
     <input id="install_button" type="submit" name="civisetup[action][Install]"
            value="<?php echo htmlentities(ts('Install CiviCRM')); ?>"
            onclick="document.getElementById('saving_top').style.display = ''; this.value = '<?php echo ts('Installing CiviCRM...', array('escape' => 'js')); ?>'"/>
-
-    <span id="saving_top" style="display: none">
-  &nbsp;
-    <img src=<?php echo $installURLPath . "network-save.gif" ?>/>
+    <span id="saving_top" style="display: none;">
+  &nbsp;   <img src="<?php echo htmlentities($installURLPath . "network-save.gif") ?>"/>
       <?php echo ts('(this will take a few minutes)'); ?>
-  </span>
+    </span>
   </div>
 
 </form>
