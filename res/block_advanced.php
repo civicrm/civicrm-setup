23c6f0f4bd514271fdc02db309834d91955fa79c
Tim Otten <totten@civicrm.org>
2018-01-15 22:08:41 -0800

    Show a summary of installation details (paths/DBs)

--- res/block_advanced.php
+++ res/block_advanced.php
@@ -1,6 +1,37 @@
 <?php \Civi\Setup::assertRunning(); ?>
-<h2><?php echo ts('Advanced'); ?></h2>
+<h2><?php echo ts('Details'); ?></h2>
 
 <p>
-  <?php echo ts('Need more advanced options -- such as customizing the database credentials? You may alternatively use the <a href="%1" target="%2">command-line installer</a>.', array(1 => 'https://github.com/civicrm/cv', 2 => '_blank')); ?>
+  <?php echo ts('The system settings were auto-detected. CiviCRM will be installed with:'); ?>
+</p>
+
+<div style="">
+  <table class="settingsTable">
+    <tbody>
+    <tr>
+      <th><?php echo ts('CMS Database'); ?></th>
+      <td>
+        <code><?php echo htmlentities('mysql://' . $model->cmsDb['username'] . ':CENSORED@' . $model->cmsDb['server'] . '/' . $model->cmsDb['database']); ?></code>
+      </td>
+    </tr>
+    <tr>
+      <th><?php echo ts('CiviCRM Database'); ?></th>
+      <td>
+        <code><?php echo htmlentities('mysql://' . $model->db['username'] . ':CENSORED@' . $model->db['server'] . '/' . $model->db['database']); ?></code>
+      </td>
+    </tr>
+    <tr>
+      <th><?php echo ts('CiviCRM Settings File'); ?></th>
+      <td><code><?php echo htmlentities($model->settingsPath); ?></code></td>
+    </tr>
+    <tr>
+      <th><?php echo ts('CiviCRM Source Code'); ?></th>
+      <td><code><?php echo htmlentities($model->srcPath); ?></code></td>
+    </tr>
+    </tbody>
+  </table>
+</div>
+
+<p style="background: #ffb; padding: 0.5em; margin: 1em auto; width: 50%">
+  <?php echo ts('Need more advanced control? You may alternatively use the <a href="%1" target="%2">command-line installer</a>.', array(1 => 'https://github.com/civicrm/cv', 2 => '_blank')); ?>
 </p>
