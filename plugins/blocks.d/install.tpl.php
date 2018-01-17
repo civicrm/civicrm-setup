7754ca68f72ee5b4a30a29605cd2e20dba4f977b
Tim Otten <totten@civicrm.org>
2018-01-16 18:36:56 -0800

    blocks.d/install - Make the layout a little less jumpy
    When you click install, don't make things jump around quite so much

--- plugins/blocks.d/install.tpl.php
+++ plugins/blocks.d/install.tpl.php
@@ -3,8 +3,8 @@
   <input id="install_button" type="submit" name="civisetup[action][Install]"
          value="<?php echo htmlentities(ts('Install CiviCRM')); ?>"
          onclick="document.getElementById('saving_top').style.display = ''; this.value = '<?php echo ts('Installing CiviCRM...', array('escape' => 'js')); ?>'"/>
-  <span id="saving_top" style="display: none;">
+  <div id="saving_top" style="display: none;">
 &nbsp;   <img src="<?php echo htmlentities($installURLPath . "network-save.gif") ?>"/>
     <?php echo ts('(this will take a few minutes)'); ?>
-</span>
+  </div>
 </div>
