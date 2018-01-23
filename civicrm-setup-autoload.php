e7991aff516c3fff952bed8f90832804a134358a
Tim Otten <totten@civicrm.org>
2018-01-22 22:26:55 -0800

    civicrm-setup-autoload.php - Make redundant loading safe

--- civicrm-setup-autoload.php
+++ civicrm-setup-autoload.php
@@ -1,24 +1,26 @@
 <?php
 
-function _civicrm_setup_autoload($class) {
-  // Facade class
-  if ($class === 'Civi\\Setup') {
-    require __DIR__ . '/src/Setup.php';
-    return;
-  }
+if (!function_exists('_civicrm_setup_autoload')) {
+  function _civicrm_setup_autoload($class) {
+    // Facade class
+    if ($class === 'Civi\\Setup') {
+      require __DIR__ . '/src/Setup.php';
+      return;
+    }
 
-  // All other classes
+    // All other classes
 
-  $prefix = 'Civi\\Setup\\';
-  $base_dir = __DIR__ . '/src/Setup/';
-  $len = strlen($prefix);
-  if (strncmp($prefix, $class, $len) !== 0) {
-    return;
+    $prefix = 'Civi\\Setup\\';
+    $base_dir = __DIR__ . '/src/Setup/';
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+      return;
+    }
+    $relative_class = substr($class, $len);
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    if (file_exists($file)) {
+      require $file;
+    }
   }
-  $relative_class = substr($class, $len);
-  $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-  if (file_exists($file)) {
-    require $file;
-  }
+  spl_autoload_register('_civicrm_setup_autoload');
 }
-spl_autoload_register('_civicrm_setup_autoload');
