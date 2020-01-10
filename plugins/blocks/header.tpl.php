7e4b52380c077bd5f6ebd01413b0380a6c86ea4b
Jade Gaunt <jadegaunt1998@outlook.com>
2020-01-10 13:03:24 +0000

    Add files via upload

--- plugins/blocks/header.tpl.php
+++ plugins/blocks/header.tpl.php
@@ -1,9 +1,13 @@
 <?php if (!defined('CIVI_SETUP')): exit("Installation plugins must only be loaded by the installer.\n"); endif; ?>
-<div class="civicrm-logo"><strong><?php echo ts('Version %1', array(1 => "{$civicrm_version} {$model->cms}")); ?></strong><br/>
-    <span><img src=<?php echo $installURLPath . "block_small.png"?> /></span>
-  </div>
-
-<h1><?php echo ts("CiviCRM Installer"); ?></h1>
+<div class="civicrm-setup-header">
+	<div class="title">
+		<h1><?php echo ts("Thanks for choosing CiviCRM. You're nearly there!"); ?><hr></h1>
+	</div>
+	<div class="civicrm-logo"><strong><?php echo ts('Version %1', array(1 => "{$civicrm_version} {$model->cms}")); ?></strong>
+	    <span><img src=<?php echo $installURLPath . "updated-logo.jpg"?> /></span>
+	 </div>
+</div>
+<h2><?php echo ts("CiviCRM Installer"); ?></h2>
 
 <noscript>
 <p class="error"><?php echo ts("Error: Javascipt appears to be disabled. The CiviCRM web-based installer requires Javascript.");?></p>
