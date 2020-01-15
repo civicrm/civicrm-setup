8807bda512156ff06e25622f60ae7f1990d95c5c
Tim Otten <totten@civicrm.org>
2020-01-14 18:35:47 -0800

    GenerateSiteKey.civi-setup.php - Remove dependency on civicrm-core class

--- plugins/installFiles/GenerateSiteKey.civi-setup.php
+++ plugins/installFiles/GenerateSiteKey.civi-setup.php
@@ -13,6 +13,23 @@ if (!defined('CIVI_SETUP')) {
   ->addListener('civi.setup.installFiles', function (\Civi\Setup\Event\InstallFilesEvent $e) {
     \Civi\Setup::log()->info(sprintf('[%s] Handle %s', basename(__FILE__), 'installFiles'));
 
-    $e->getModel()->siteKey = \CRM_Utils_String::createRandom(32, \CRM_Utils_String::ALPHANUMERIC);
+    $toAlphanum = function($bits) {
+      return preg_replace(';[^a-zA-Z0-9];', '', base64_encode($bits));
+    };
+
+    if (!empty($e->getModel()->siteKey)) {
+      // skip
+    }
+    elseif (function_exists('random_bytes')) {
+      $e->getModel()->siteKey = $toAlphanum(random_bytes(32));
+    }
+    elseif (function_exists('openssl_random_pseudo_bytes')) {
+      $e->getModel()->siteKey = $toAlphanum(openssl_random_pseudo_bytes(32));
+    }
+    else {
+      throw new \RuntimeException("Failed to generate a random site key");
+    }
+
+    \Civi\Setup::log()->info(sprintf('[%s] Done %s', basename(__FILE__), 'installFiles'));
 
   }, \Civi\Setup::PRIORITY_PREPARE);
